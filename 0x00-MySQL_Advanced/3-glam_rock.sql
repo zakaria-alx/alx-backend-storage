217781273b840db719f6439141c56cf90183c20d
zakaria-alx <125534771+zakaria-alx@users.noreply.github.com>
2024-01-18 17:14:35 +0000

    0x00-MySQL_Advanced

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,4 @@
--- Lists all bands with Glam rock as their main style, ranked by their longevity.
--- SELECT band_name, (IFNULL(split, YEAR(CURRENT_DATE())) - formed) AS lifespan
-SELECT band_name, (IFNULL(split, '2020') - formed) AS lifespan
-    FROM metal_bands
-    WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
-    ORDER BY lifespan DESC;
+-- lists bands with glam rock and orders by their longevity
+SELECT band_name, IFNULL(split, 2020) - IFNULL(formed, 0) AS lifespan 
+FROM metal_bands 
+WHERE style LIKE '%Glam rock%';
