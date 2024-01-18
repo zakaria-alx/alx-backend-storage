06c75f14ef3a4ed9e7a41826e4e35cf65f60a567
zakaria-alx <125534771+zakaria-alx@users.noreply.github.com>
2024-01-18 18:13:19 +0100

    Update 3-glam_rock.sql

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,4 @@
 -- lists bands with glam rock and orders by their longevity
-SELECT band_name, (IFNULL(split, 2022) - formed) AS lifespan
-FROM metal_bands
-WHERE style LIKE '%Glam rock%'
-ORDER BY lifespan ASC;
+SELECT band_name, IFNULL(split, 2020) - IFNULL(formed, 0) AS lifespan 
+FROM metal_bands 
+WHERE style LIKE '%Glam rock%';
